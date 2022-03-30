62f196358d42d4094888269df46196c08042dfe4
Matt Crecraft <mcrecraft@gmail.com>
2022-03-30 11:39:05 -0500

    Completed Exercise 2

--- 2.js
+++ 2.js
@@ -9,6 +9,10 @@ Given an array containing the english alphabet, write code that returns an array
 function getAlphaNumericAlphabet(){
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   let alphaNumericAlphabet
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    alphaNumericAlphabet = [...numbers, ...alphabet]
+    alphaNumericAlphabet.push("&")
+    
 
   // Your code here
 
