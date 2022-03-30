b9f581290507a8d8dd931bd3060fa7d18a07a4b0
Matt Crecraft <mcrecraft@gmail.com>
2022-03-30 11:25:29 -0500

    Completed Exercise 1

--- 1.js
+++ 1.js
@@ -14,7 +14,7 @@ Note:
 
 function getVowels(){
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  let vowels
+  let vowels = [alphabet[0], alphabet[4], alphabet[8], alphabet[14], alphabet[20], alphabet[24]]
 
   // Your code here
 
